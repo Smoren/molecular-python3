9a27b5dbd6b16a593621a6aebedfe99e20938b3a
Smoren <ofigate@gmail.com>
2023-11-15 23:01:07 +0300

    upd

--- app/storage.py
+++ app/storage.py
@@ -36,57 +36,6 @@ class Storage:
             np.repeat(0, size),
         ], dtype=np.float64).T
 
-    @staticmethod
-    def interact_step_old(cluster_atoms: np.ndarray, neighbour_atoms: np.ndarray, cluster_mask):
-        for atom in cluster_atoms:
-            d = np.array([
-                neighbour_atoms[:, AtomField.X] - atom[AtomField.X],
-                neighbour_atoms[:, AtomField.Y] - atom[AtomField.Y]]
-            ).T
-
-            l = np.linalg.norm(d, axis=1)
-
-            du = (d.T / l).T
-            du[np.isnan(du)] = 0
-
-            dv = (du.T / l).T
-            dv[np.isnan(dv)] = 0
-            dv = np.sum(dv, axis=0) * 4
-
-            atom[AtomField.VX] += dv[0]
-            atom[AtomField.VY] += dv[1]
-
-        return cluster_atoms, cluster_mask
-
-    @staticmethod
-    def interact_step(
-        cluster_coords: Tuple[int, ...],
-        cluster_atoms_shape: Tuple[int, ...],
-        neighbour_atoms_shape: Tuple[int, ...],
-    ):
-        cluster_atoms, shm1 = get_shared_variable(cluster_coords, cluster_atoms_shape, 'cluster_atoms')
-        neighbour_atoms, shm2 = get_shared_variable(cluster_coords, neighbour_atoms_shape, 'neighbour_atoms')
-
-        for atom in cluster_atoms:
-            d = np.array([
-                neighbour_atoms[:, AtomField.X] - atom[AtomField.X],
-                neighbour_atoms[:, AtomField.Y] - atom[AtomField.Y]]
-            ).T
-
-            l = np.linalg.norm(d, axis=1)
-
-            du = (d.T / l).T
-            du[np.isnan(du)] = 0
-
-            dv = (du.T / l).T
-            dv[np.isnan(dv)] = 0
-            dv = np.sum(dv, axis=0) * 4
-
-            atom[AtomField.VX] += dv[0]
-            atom[AtomField.VY] += dv[1]
-
-        return cluster_coords, shm1, shm2
-
     def interact(self) -> None:
         clusters_coords = np.unique(self.data[:, [AtomField.CLUSTER_X, AtomField.CLUSTER_Y]], axis=0)
 
@@ -120,7 +69,7 @@ class Storage:
 
             tasks_data.append((cluster_coords_tuple, cluster_atoms.shape, neighbour_atoms.shape))
 
-        task_results = self._pool.starmap(self.interact_step, tasks_data)
+        task_results = self._pool.starmap(self._interact_step, tasks_data)
         for task_result in task_results:
             cluster_coords_tuple, shm1, shm2 = task_result
             cluster_atoms, shm = get_shared_variable(
@@ -145,3 +94,32 @@ class Storage:
 
         self.data[:, AtomField.CLUSTER_X] = np.floor(self.data[:, AtomField.X] / self._cluster_size)
         self.data[:, AtomField.CLUSTER_Y] = np.floor(self.data[:, AtomField.Y] / self._cluster_size)
+
+    @staticmethod
+    def _interact_step(
+        cluster_coords: Tuple[int, ...],
+        cluster_atoms_shape: Tuple[int, ...],
+        neighbour_atoms_shape: Tuple[int, ...],
+    ):
+        cluster_atoms, shm1 = get_shared_variable(cluster_coords, cluster_atoms_shape, 'cluster_atoms')
+        neighbour_atoms, shm2 = get_shared_variable(cluster_coords, neighbour_atoms_shape, 'neighbour_atoms')
+
+        for atom in cluster_atoms:
+            d = np.array([
+                neighbour_atoms[:, AtomField.X] - atom[AtomField.X],
+                neighbour_atoms[:, AtomField.Y] - atom[AtomField.Y]]
+            ).T
+
+            l = np.linalg.norm(d, axis=1)
+
+            du = (d.T / l).T
+            du[np.isnan(du)] = 0
+
+            dv = (du.T / l).T
+            dv[np.isnan(dv)] = 0
+            dv = np.sum(dv, axis=0) * 4
+
+            atom[AtomField.VX] += dv[0]
+            atom[AtomField.VY] += dv[1]
+
+        return cluster_coords, shm1, shm2
