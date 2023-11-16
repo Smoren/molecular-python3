033a541997f2e80951cefee7cd01e60b0228090c
Smoren <ofigate@gmail.com>
2023-11-16 21:24:13 +0300

    upd

--- app/storage.py
+++ app/storage.py
@@ -10,18 +10,42 @@ class Storage:
     data: np.ndarray
     _max_coord: np.ndarray
     _cluster_size: int
+    _gravity: np.ndarray
+    _link_gravity: np.ndarray
+    _links: np.array
+    _type_links: np.array
+    _colors: np.array
 
     def __init__(self, size: int, max_coord: Tuple[int, int], cluster_size: int):
         self._max_coord = np.array(max_coord)
         self._cluster_size = cluster_size
+
+        self._gravity = np.array([
+            [-1, -1, -1],
+            [-1, -1, -1],
+            [-1, -1, -1],
+        ])
+        self._link_gravity = np.array([
+            [-1, -1, 1],
+            [-1, -1, -1],
+            [-1, -1, -1],
+        ])
+        self._links = np.array([1, 3, 2])
+        self._type_links = np.array([
+            [0, 1, 1],
+            [1, 2, 1],
+            [1, 1, 2],
+        ])
+
         self.data = np.array([
             np.random.randint(low=0, high=max_coord[0], size=size).astype('float'),
             np.random.randint(low=0, high=max_coord[1], size=size).astype('float'),
             np.random.randint(low=-10, high=10, size=size).astype('float'),
             np.random.randint(low=-10, high=10, size=size).astype('float'),
-            np.repeat(1, size),
-            np.repeat(0, size),
-            np.repeat(0, size),
+            np.repeat(1, size).astype('float'),
+            np.repeat(0, size).astype('float'),
+            np.repeat(0, size).astype('float'),
+            np.random.randint(low=0, high=3, size=size).astype('float'),
         ], dtype=np.float64).T
 
     def interact(self) -> None:
